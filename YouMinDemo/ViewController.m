0da1bbd7ef920b7d6f688278a91ab0b29cf75c28
pandazheng <543828315@qq.com>
2015-11-03 17:12:43 +0800

    增加MAC地址
    增加MAC地址

--- YouMinDemo/ViewController.m
+++ YouMinDemo/ViewController.m
@@ -14,6 +14,8 @@
 #import <sys/utsname.h>
 #import <sys/socket.h>
 #import <netinet/in.h>
+#import <net/if.h>
+#import <net/if_dl.h>
 #import <netinet6/in6.h>
 #import <arpa/inet.h>
 #import <ifaddrs.h>
@@ -236,6 +238,31 @@
     //NSLog(@"-- serialNumber: %@", [AADeviceInfo serialNumber]);
 }
 
+- (void) getBatteryInfo {
+    //电池的状态是可监听的
+    [UIDevice currentDevice].batteryMonitoringEnabled = YES;
+    //电池的状态
+    switch ([UIDevice currentDevice].batteryState) {
+        case UIDeviceBatteryStateUnknown:
+            NSLog(@"电池的状态未知");
+            break;
+        case UIDeviceBatteryStateCharging:
+            NSLog(@"电池正在充电");
+            break;
+        case UIDeviceBatteryStateUnplugged:
+            NSLog(@"电池未充电");
+            break;
+        case UIDeviceBatteryStateFull:
+            NSLog(@"电池电量充满");
+            break;
+        default:
+            break;
+    }
+    
+    //获取当前电池的电量
+    NSLog(@"当前电池的电量百分比是%3.0f%%",([[UIDevice currentDevice] batteryLevel])*100);
+}
+
 - (void) getIphoneDeviceInfo {
     UIDevice *device = [UIDevice currentDevice];
     //平台型号
@@ -262,6 +289,123 @@
     NSLog(@"%@",[NSString stringWithFormat:@"手机是否越狱:%hhd",[device isJailBreak]]);
 }
 
+//-(NSString *) macaddress
+//{
+//    int                 mgmtInfoBase[6];
+//    char                *msgBuffer = NULL;
+//    size_t              length;
+//    unsigned char       macAddress[6];
+//    struct if_msghdr    *interfaceMsgStruct;
+//    struct sockaddr_dl  *socketStruct;
+//    NSString            *errorFlag = NULL;
+//    
+//    // Setup the management Information Base (mib)
+//    mgmtInfoBase[0] = CTL_NET;        // Request network subsystem
+//    mgmtInfoBase[1] = AF_ROUTE;       // Routing table info
+//    mgmtInfoBase[2] = 0;
+//    mgmtInfoBase[3] = AF_LINK;        // Request link layer information
+//    mgmtInfoBase[4] = NET_RT_IFLIST;  // Request all configured interfaces
+//    
+//    // With all configured interfaces requested, get handle index
+//    if ((mgmtInfoBase[5] = if_nametoindex("en0")) == 0)
+//        errorFlag = @"if_nametoindex failure";
+//    else
+//    {
+//        // Get the size of the data available (store in len)
+//        if (sysctl(mgmtInfoBase, 6, NULL, &length, NULL, 0) < 0)
+//            errorFlag = @"sysctl mgmtInfoBase failure";
+//        else
+//        {
+//            // Alloc memory based on above call
+//            msgBuffer =(char*) malloc(length) ;
+//            if ((msgBuffer) == NULL)
+//                errorFlag = @"buffer allocation failure";
+//            else
+//            {
+//                // Get system information, store in buffer
+//                if (sysctl(mgmtInfoBase, 6, msgBuffer, &length, NULL, 0) < 0)
+//                    errorFlag = @"sysctl msgBuffer failure";
+//            }
+//        }
+//    }
+//    
+//    // Befor going any further...
+//    if (errorFlag != NULL)
+//    {
+//        //  DLog(@"Error: %@", errorFlag);
+//        return errorFlag;
+//    }
+//    
+//    // Map msgbuffer to interface message structure
+//    interfaceMsgStruct = (struct if_msghdr *) msgBuffer;
+//    
+//    // Map to link-level socket structure
+//    socketStruct = (struct sockaddr_dl *) (interfaceMsgStruct + 1);
+//    
+//    // Copy link layer address data in socket structure to an array
+//    memcpy(&macAddress, socketStruct->sdl_data + socketStruct->sdl_nlen, 6);
+//    
+//    // Read from char array into a string object, into traditional Mac address format
+//    NSString *macAddressString = [NSString stringWithFormat:@"%02X%02X%02X%02X%02X%02X",
+//                                  macAddress[0], macAddress[1], macAddress[2],
+//                                  macAddress[3], macAddress[4], macAddress[5]];
+//    //NSLog(@"Mac Address: %@", macAddressString);
+//    
+//    // Release the buffer memory
+//    free(msgBuffer);
+//    
+//    return macAddressString;
+//}
+- (NSString *) macaddress
+{
+    
+    int                 mib[6];
+    size_t              len;
+    char                *buf;
+    unsigned char       *ptr;
+    struct if_msghdr    *ifm;
+    struct sockaddr_dl  *sdl;
+    
+    mib[0] = CTL_NET;
+    mib[1] = AF_ROUTE;
+    mib[2] = 0;
+    mib[3] = AF_LINK;
+    mib[4] = NET_RT_IFLIST;
+    
+    if ((mib[5] = if_nametoindex("en0")) == 0) {
+        printf("Error: if_nametoindex error/n");
+        return NULL;
+    }
+    
+    if (sysctl(mib, 6, NULL, &len, NULL, 0) < 0) {
+        printf("Error: sysctl, take 1/n");
+        return NULL;
+    }
+    
+    if ((buf = malloc(len)) == NULL) {
+        printf("Could not allocate memory. error!/n");
+        return NULL;
+    }
+    
+    if (sysctl(mib, 6, buf, &len, NULL, 0) < 0) {
+        printf("Error: sysctl, take 2");
+        return NULL;
+    }
+    
+    ifm = (struct if_msghdr *)buf;
+    sdl = (struct sockaddr_dl *)(ifm + 1);
+    ptr = (unsigned char *)LLADDR(sdl);
+    NSString *outstring = [NSString stringWithFormat:@"%02x:%02x:%02x:%02x:%02x:%02x", *ptr, *(ptr+1), *(ptr+2), *(ptr+3), *(ptr+4), *(ptr+5)];
+    
+    //    NSString *outstring = [NSString stringWithFormat:@"%02x%02x%02x%02x%02x%02x", *ptr, *(ptr+1), *(ptr+2), *(ptr+3), *(ptr+4), *(ptr+5)];
+    
+    //NSLog(@"outString:%@", outstring);
+    
+    free(buf);
+    
+    return [outstring uppercaseString];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
@@ -275,9 +419,15 @@
     NSString *deviceName = [self deviceName];
     NSLog(@"手机类型:%@",deviceName);
     
+    NSLog(@"手机Model:%@",[UIDevice currentDevice].model);
+    
     [self defaultGateWay];
     
     [self getIphoneDeviceInfo];
+    [self getBatteryInfo];
+    
+    NSString *macaddress = [self macaddress];
+    NSLog(@"手机的MAC地址:%@",macaddress);
 }
 
 - (void)didReceiveMemoryWarning {
